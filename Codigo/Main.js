12938679c621190b89aae87e570742e507a97414
Almir Ferreira <almirferreira.sj@gmail.com>
2021-11-21 23:55:29 -0300

    Adicionando card de detalhes

---
+++ Codigo/Main.js
@@ -0,0 +1,32 @@
+let exp1 = "minha experiencia 1";
+let exp2 = "minha experiencia 2";
+
+function info(opcao){
+    let cardInfo = document.createElement('div');
+    let titulo = document.createElement('h2');
+    let texto = opcao == 1 ? document.createTextNode(exp1) : document.createTextNode(exp2);
+    let container = document.createElement('div');
+    let botao = document.createElement('button');
+
+
+    cardInfo.setAttribute('class', 'info');
+    container.setAttribute('id', 'containerInfo');
+    botao.textContent = 'Sair';
+    botao.addEventListener('click', function(){ console.log('remover'); let conteudo = document.getElementById('containerInfo'); conteudo.parentNode.removeChild(conteudo);});
+    titulo.textContent = "Detalhes";
+
+    cardInfo.appendChild(titulo);
+    cardInfo.appendChild(texto);
+    cardInfo.appendChild(botao);
+    container.appendChild(cardInfo);
+
+    console.log(texto);
+    document.body.appendChild(container);
+}
+info(2)
+function remover(){
+    console.log('remover');
+    let conteudo = document.getElementById('containerInfo');
+    console.log(conteudo)
+    //conteudo.style.zIndex = '9';
+}
